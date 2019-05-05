12f008c88f4fe9501db78bfb041a78f68ce58836
grafkendo <grafdev@live.com>
2019-05-04 22:42:07 -0700

    post hack commit

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -1,23 +1,16 @@
-<?php 
-
-
-
-
-
-
-
+<?php
 /**
  * Loads the WordPress environment and template.
  *
  * @package WordPress
  */
 
-if ( !isset($wp_did_header) ) {
+if ( ! isset( $wp_did_header ) ) {
 
 	$wp_did_header = true;
 
 	// Load the WordPress library.
-	require_once( dirname(__FILE__) . '/wp-load.php' );
+	require_once( dirname( __FILE__ ) . '/wp-load.php' );
 
 	// Set up the WordPress query.
 	wp();
